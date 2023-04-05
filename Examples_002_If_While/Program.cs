89eba620ccb2535512623c2487f547ccde2a64d3
Maks Chaliy <mrgoodchaliy@yandex.ru>
2023-04-05 21:51:10 +0300

    added Examples_002_If_While

---
+++ Examples_002_If_While/Program.cs
@@ -0,0 +1,32 @@
+﻿
+//========== перебор от ...до... ==========
+/*
+for (double i = 0; i <= 10; i++)
+{
+  Console.WriteLine($"Number: {i}");
+}
+*/
+
+//========== перебор от ...до... ==========
+/*
+int num = 0;
+while (num <= 10)
+{
+  Console.WriteLine($"Number: {num}");
+  num++;
+}
+*/
+
+//========== перебор от ...до... с флагом остановки ==========
+/*
+bool isHappy = true;
+while(isHappy)
+{
+  Console.WriteLine("Сколько будет 2 + 2 * 2?: ");
+  int number = int.Parse(Console.ReadLine());
+  if(number == 6)
+  {
+    isHappy = false;
+  }
+}
+*/
