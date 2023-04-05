cca2391ce19103b5194f6c1a8997d5d6a6ad3d56
Maks Chaliy <mrgoodchaliy@yandex.ru>
2023-04-05 19:09:45 +0300

    added Examples_001_Math

---
+++ Examples_001_Math/Program.cs
@@ -0,0 +1,21 @@
+﻿
+// из отрицательного делает положительное число
+Console.WriteLine(Math.Abs(-20));//20
+
+// округляет в большую сторону не зависимо от остатка
+Console.WriteLine(Math.Ceiling(4.11));//5
+
+// округляет в меньшуу сторону не зависимо от остатка
+Console.WriteLine(Math.Floor(4.89));//4
+
+// округляет в меньшую или большую в зависимости от остатка
+Console.WriteLine(Math.Round(6.46));//6
+
+// поиск максимального из двух
+Console.WriteLine(Math.Max(5, 0));//5
+
+// поиск минимального из двух
+Console.WriteLine(Math.Min(5, 0));//0
+
+// возведенеие в степень
+Console.WriteLine(Math.Pow(5, 3));//125
